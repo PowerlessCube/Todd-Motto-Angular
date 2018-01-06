3a95d4635095a6f51d1ffefc94b1bd461b334a50
Alistair Mackay <alistair.mackay.work@gmail.com>
2018-01-06 23:29:15 +0000

    Creating a custom attribute

--- 22-custom-attribute-directive/app/app.component.ts
+++ 22-custom-attribute-directive/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // Create our own custom directive called "credit-card"
   template: `
     <div>
       <label>
--- 22-custom-attribute-directive/app/credit-card/credit-card.directive.ts
+++ 22-custom-attribute-directive/app/credit-card/credit-card.directive.ts
@@ -1,10 +1,17 @@
+// 1. import a directive
 import { Directive, ElementRef } from '@angular/core';
 
+// 3. add a decorator, that takes a configuration object.
 @Directive({
+  // 4. create an attribute selector square brackets denotes that it's an element attribute
   selector: '[credit-card]'
 })
+// 2. Create our CreditCard directive class
 export class CreditCardDirective {
+  // 5. injecting stuff into construct
   constructor(private element: ElementRef) {
     console.log(this.element);
   }
 }
+
+// Note - the only thing you can't do inside a directive is to declare a template.

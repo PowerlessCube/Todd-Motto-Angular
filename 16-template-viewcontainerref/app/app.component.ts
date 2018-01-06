e503a7ce90ea66148e3f40f00b7de91b237ba00b
Alistair Mackay <alistair.mackay.work@gmail.com>
2018-01-06 20:26:47 +0000

    Dynamic template rendering with ViewContainerRef

--- 16-template-viewcontainerref/app/app.component.ts
+++ 16-template-viewcontainerref/app/app.component.ts
@@ -6,6 +6,7 @@ import { User } from './auth-form/auth-form.interface';
 
 @Component({
   selector: 'app-root',
+  // Learn how to dynamically use a custome template.
   template: `
     <div>
       <div #entry></div>
@@ -18,9 +19,12 @@ import { User } from './auth-form/auth-form.interface';
 export class AppComponent implements AfterContentInit {
 
   @ViewChild('entry', { read: ViewContainerRef }) entry: ViewContainerRef;
+  // Using our template reference and importing TemplateRef<any>
+  // Instantiate the custom template.
   @ViewChild('tmpl') tmpl: TemplateRef<any>;
 
   ngAfterContentInit() {
+    // Inject it into the entry div 
     this.entry.createEmbeddedView(this.tmpl);
   }
 

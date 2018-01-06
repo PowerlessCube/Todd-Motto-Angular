4b2fc9a6041e5d24366c6e78f8e8534668f656bc
Alistair Mackay <alistair.mackay.work@gmail.com>
2018-01-06 22:49:44 +0000

    Dynamic template rendering with ngTemplateOutlet

--- 18-ngtemplateoutlet/app/app.component.ts
+++ 18-ngtemplateoutlet/app/app.component.ts
@@ -1,3 +1,4 @@
+// Change the container div to use a directive called ng-template outlet
 import { Component } from '@angular/core';
 
 import { AuthFormComponent } from './auth-form/auth-form.component';
@@ -6,6 +7,8 @@ import { User } from './auth-form/auth-form.interface';
 
 @Component({
   selector: 'app-root',
+  // ng-container is a simple container that doesn't get rendered to the dom.
+  // Use ngTemplateOutlet directive for passing the template ref into it.
   template: `
     <div>
       <ng-container

7d1580ef64047b81e55bfb18ba835664a65c9b16
Alistair Mackay <alistair.mackay.work@gmail.com>
2018-01-07 16:28:02 +0000

    Implementing a ControlValueAccessor

--- 29-reactive-setup/app/stock-inventory/components/stock-counter/stock-counter.component.ts
+++ 29-reactive-setup/app/stock-inventory/components/stock-counter/stock-counter.component.ts
@@ -1,7 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, forwardRef } from '@angular/core';
+// New imports from forms
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms'
+
+// Overall-goal: Register our counter component and extend NG Accessor to allow our component to talk to the form groups
+// Setup our custom Provider - registers our stock counter component
+const COUNTER_CONTROL_ACCESSOR = {
+  // Extends the NG_VALUE_ACCESSOR
+  provide: NG_VALUE_ACCESSOR,
+  // ForwardRef is allowing us to hoist component
+  useExisting: forwardRef(() => StockCounterComponent),
+  multi: true
+};
+
+
 
 @Component({
   selector: 'stock-counter',
+  // Implement it in the provider
+  providers: [COUNTER_CONTROL_ACCESSOR],
   styleUrls: ['stock-counter.component.scss'],
   template: `
     <div class="stock-counter">
@@ -28,7 +44,30 @@ import { Component, Input } from '@angular/core';
   `
 })
 
-export class StockCounterComponent {
+// Implement ControlValueAccessor
+export class StockCounterComponent implements ControlValueAccessor {
+  // Create private values
+  private onTouch: Function;
+  private onModelChange: Function;
+
+  // Required functions of the implementation of ControlValueAccessor
+  registerOnTouched(fn) {
+    // Assigning to our internal function
+    this.onTouch = fn
+  }
+
+  registerOnChange(fn) {
+    // called when our model has changed.
+    this.onModelChange = fn;
+  }
+
+  // Any initial value or changed value
+  writeValue(value){
+    // reassigns to our value.
+    this.value = value || 0;
+  }
+  // till here.
+
   @Input() step: number = 10;
   @Input() min: number = 10;
   @Input() max: number = 1000;
@@ -38,12 +77,18 @@ export class StockCounterComponent {
   increment() {
     if (this.value < this.max) {
       this.value = this.value + this.step;
+      // passes the value into the Model change
+      this.onModelChange(this.value);
     }
+    // Notifys the Control that our component has been interacted with
+    this.onTouch();
   }
 
   decrement() {
     if (this.value > this.min) {
       this.value = this.value - this.step;
+      this.onModelChange(this.value);
     }
+    this.onTouch();
   }
 }

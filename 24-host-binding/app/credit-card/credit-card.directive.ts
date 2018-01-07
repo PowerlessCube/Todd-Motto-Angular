a612f2aa3f9a8c1092d88684a4ba2a5c694834ef
Alistair Mackay <alistair.mackay.work@gmail.com>
2018-01-07 00:00:32 +0000

    Understanding @HostBinding

--- 24-host-binding/app/credit-card/credit-card.directive.ts
+++ 24-host-binding/app/credit-card/credit-card.directive.ts
@@ -1,12 +1,15 @@
+// Add HostBinding
 import { Directive, HostListener, HostBinding, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[credit-card]'
 })
 export class CreditCardDirective {
-
+  // HostBinding = allows us to communicate with our host node and change the value of a property.
   @HostBinding('style.border')
   border: string;
+  // you could bind classes to your host DOM element:
+  // @HostBinding('class') classes = 'a b c'
 
   @HostListener('input', ['$event'])
   onKeyDown(event: KeyboardEvent) {
@@ -24,8 +27,11 @@ export class CreditCardDirective {
 
     input.value = numbers.join(' ');
 
+    // empty the string
     this.border = '';
+    // use the test method on our trimmed.
     if (/[^\d]+/.test(trimmed)) {
+      // means the border is solid red if invalid.
       this.border = '1px solid red';
     }
 

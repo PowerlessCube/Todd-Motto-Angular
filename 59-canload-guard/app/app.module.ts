b0ebcc3bf969e712c2a57ccd6ce63d6b8fed078e
PowerlessCube <alistair.mackay@escrivo.com>
2018-01-11 16:21:24 +0000

    Protecting lazy-loaded modules with canLoad

--- 59-canload-guard/app/app.module.ts
+++ 59-canload-guard/app/app.module.ts
@@ -1,3 +1,4 @@
+// Setting up a Routing guard to prevent a user with out access from hitting routes they are not suppose too.
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
@@ -5,12 +6,16 @@ import { HttpModule } from '@angular/http';
 
 import { MailModule } from './mail/mail.module';
 import { AuthModule } from './auth/auth.module';
+// import our Authguard.
 import { AuthGuard } from './auth/auth.guard';
 
 import { AppComponent } from './app.component';
 
+// Our Auth guard gets called  before the route gets loaded.
 export const ROUTES: Routes = [
+  // add canLoad which accepts an array and pass in our guard
   { path: 'dashboard', canLoad: [AuthGuard], loadChildren: './dashboard/dashboard.module#DashboardModule' },
+  // canload lets us decide if the current user is allowed to load our module.
   { path: '**', redirectTo: 'mail/folder/inbox' }
 ];
 
